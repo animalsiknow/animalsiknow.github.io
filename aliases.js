0a1860b2a0d88c66aa4ec069fd7d0c9faf9dbff9
Simon Génier <1782845+animalsiknow@users.noreply.github.com>
2019-01-15 16:30:42 -0500

    Documentation for ei-sys.

---
+++ aliases.js
@@ -0,0 +1,3 @@
+var ALIASES = {};
+ALIASES['ei_sys'] = {};
+ALIASES['libc'] = {};
---
+++ implementors/core/ops/bit/trait.BitOrAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ei_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/bit/trait.BitOrAssign.html\" title=\"trait core::ops::bit::BitOrAssign\">BitOrAssign</a>&lt;<a class=\"struct\" href=\"ei_sys/struct.erlang_char_encoding.html\" title=\"struct ei_sys::erlang_char_encoding\">erlang_char_encoding</a>&gt; for <a class=\"struct\" href=\"ei_sys/struct.erlang_char_encoding.html\" title=\"struct ei_sys::erlang_char_encoding\">erlang_char_encoding</a>",synthetic:false,types:["ei_sys::erlang_char_encoding"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
